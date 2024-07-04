5091ae1caa15a28ad3debb1ba6b22353ddcf463e
Narek <nynarek@gmail.com>
2024-07-05 01:12:57 +0400

    change app name

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const InventoryManagementApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class InventoryManagementApp extends StatelessWidget {
+  const InventoryManagementApp({super.key});
 
   @override
   Widget build(BuildContext context) {
