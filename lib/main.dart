a6a8746356e32a925b3acdb1843f2fd525945b44
Narek <nynarek@gmail.com>
2024-07-05 01:22:34 +0400

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:ekey_flutter_task/bloc/product_bloc.dart';
-import 'package:ekey_flutter_task/data/product_data_source.dart';
 import 'package:ekey_flutter_task/pages/product_page.dart';
 import 'package:flutter/material.dart';
-
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const InventoryManagementApp());
@@ -14,13 +10,10 @@ class InventoryManagementApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Inventory Management Web Application',
       debugShowCheckedModeBanner: false,
-      home: BlocProvider(
-          create: (context) => ProductBloc(productDataSource: ProductDataSource()),
-          child: const ProductPage(),
-        ),
+      home: ProductPage(),
     );
   }
 }
